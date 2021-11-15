1fca84452e071f33095b3d9fae47be3450c1be63
waflol <lenguyenhunganh2001@gmail.com>
2021-11-15 21:43:13 +0700

    thêm ghi chú datastring

--- DBMS_Nhom3/DBLayer/DBMain.cs
+++ DBMS_Nhom3/DBLayer/DBMain.cs
@@ -8,7 +8,7 @@ namespace DBMS_Nhom3.DBLayer
 {
     class DBMain
     {
-        string Connstr = "Data Source=PC-RTX3060Ti;Initial Catalog=ShopDB;Integrated Security=True"; // thêm conn
+        string Connstr = "Data Source=PC-RTX3060Ti;Initial Catalog=ShopDB;Integrated Security=True"; // thêm conn, khi chạy cần sửa lại data string
         SqlConnection conn = null;
         SqlCommand comm = null;
         SqlDataAdapter da = null;
