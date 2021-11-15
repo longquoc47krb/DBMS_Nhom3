e23ebbf6089d152e08d47db466d0ff36f13ecb3a
waflol <lenguyenhunganh2001@gmail.com>
2021-11-15 21:40:08 +0700

    Thêm form thêm nhân viên

---
+++ DBMS_Nhom3/Add_Staff.cs
@@ -0,0 +1,38 @@
+﻿using DBMS_Nhom3.BSLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DBMS_Nhom3
+{
+    public partial class Add_Staff : Form
+    {
+        public Add_Staff()
+        {
+            InitializeComponent();
+        }
+
+        private void Yes_Button_Click(object sender, EventArgs e)
+        {
+            BLStaff staff = new BLStaff();
+            staff.addStaff(User_Textbox.Text, Pass_Textbox.Text,staffname_Textbox.Text,phone_Textbox.Text,address_Textbox.Text,position_Textbox.Text,gender_Textbox.Text);
+            this.Close();
+        }
+
+        private void Reset_Button_Click(object sender, EventArgs e)
+        {
+            User_Textbox.Text = "";
+            Pass_Textbox.Text = "";
+            Pass_Textbox.Text = "";
+            staffname_Textbox.Text = "";
+            phone_Textbox.Text = "";
+            address_Textbox.Text = "";
+            position_Textbox.Text = "";
+            gender_Textbox.Text = "";
+        }
+    }
+}
